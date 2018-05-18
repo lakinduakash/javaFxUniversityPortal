cd75ae8c3e7c8136b2a089e65ef4118925c16f6a
lakinduakash <lakinduakash@gmail.com>
2018-05-18 23:38:18 +0530

    initial commit

---
+++ src/main/java/com/ultimatex/nsbm/NsbmMain.java
@@ -0,0 +1,45 @@
+package com.ultimatex.nsbm;
+
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.controls.JFXTabPane;
+import com.ultimatex.nsbm.ui.login.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.Border;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class NsbmMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        loader.load();
+
+        LoginController lc=loader.getController();
+        primaryStage.setTitle("Login to NSBM portal");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        Scene scene = new Scene(loader.getRoot(), lc.loginPane.getPrefWidth(), lc.loginPane.getPrefHeight());
+
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ src/main/java/com/ultimatex/nsbm/Sample.java
@@ -0,0 +1,15 @@
+package com.ultimatex.nsbm;
+
+
+import javafx.fxml.FXML;
+
+public class Sample {
+
+    @FXML
+    public void startNew()
+    {
+
+    }
+
+
+}
