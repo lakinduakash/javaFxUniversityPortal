5ba2b86944a76a45216856c5fdd6a6f3fed72b81
lakinduakash <lakinduakash@gmail.com>
2018-05-19 19:00:03 +0530

    update

--- src/main/java/com/ultimatex/nsbm/NsbmMain.java
+++ src/main/java/com/ultimatex/nsbm/NsbmMain.java
@@ -12,22 +12,19 @@ public class NsbmMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //load fxml file
         FXMLLoader loader =new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         loader.load();
-        
-        //get controller class
+
         LoginController lc=loader.getController();
-        primaryStage.setTitle("Login to NSBM Portal");
+        primaryStage.setTitle("Login to NSBM portal");
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        
-        //set scene
+
         Scene scene = new Scene(loader.getRoot(), lc.loginPane.getPrefWidth(), lc.loginPane.getPrefHeight());
 
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
-        
-        //show the stage
+
+
         primaryStage.show();
     }
 
