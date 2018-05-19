17022638cb9517dfb948c8d853e38151c807943a
lakinduakash <lakinduakash@gmail.com>
2018-05-20 03:04:03 +0530

    add states

---
+++ src/main/java/com/ultimatex/nsbm/util/MainContainerState.java
@@ -0,0 +1,11 @@
+package com.ultimatex.nsbm.util;
+
+public interface MainContainerState {
+
+    int SETTINGS = 1;
+    int PROFILE = 2;
+    int PAYMENT = 3;
+    int SCHEDULE = 4;
+    int RSULT = 5;
+
+}
