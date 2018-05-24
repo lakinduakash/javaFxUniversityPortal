e7640c3ea6f41eafa3c88888f02a37c7fc0c5a52
lakinduakash <lakinduakash@gmail.com>
2018-05-24 21:45:34 +0530

    update test cases

--- src/test/java/com/ultimatex/nsbm/SessionTest.java
+++ src/test/java/com/ultimatex/nsbm/SessionTest.java
@@ -1,9 +1,13 @@
 package com.ultimatex.nsbm;
 
+import com.ultimatex.nsbm.util.DatabaseHelper;
+import com.ultimatex.nsbm.util.InvalidSessionException;
 import com.ultimatex.nsbm.util.Session;
+import com.ultimatex.nsbm.util.StudentHelper;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -15,5 +19,23 @@ public class SessionTest {
 
         assertThat(session, is(notNullValue()));
 
+    }
+
+    @Test(expected = InvalidSessionException.class)
+    public void sessionClose() {
+        Session.createInstance("email@email.com", "password");
+        Session.close();
+
+        Session.getSession();
+
+    }
+
+    @Test
+    public void getUser() {
+        Session.createInstance("email@email.com", "password");
+
+        User user = Session.getUser(Session.getSession(), DatabaseHelper.getInstance().getDatabase());
+
+        assertThat(user, is(instanceOf(StudentHelper.class)));
     }
 }
