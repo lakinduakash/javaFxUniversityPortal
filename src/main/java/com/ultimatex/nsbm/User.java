e1adde437cc2a45c8207f95d4dd04c4f4aefe463
lakinduakash <lakinduakash@gmail.com>
2018-05-24 21:50:10 +0530

    add abstract class for logged users

---
+++ src/main/java/com/ultimatex/nsbm/User.java
@@ -0,0 +1,27 @@
+package com.ultimatex.nsbm;
+
+import com.mongodb.client.MongoDatabase;
+
+public abstract class User {
+
+    public final static String TYPE_STUDENT = "student";
+    public final static String TYPE_ADMIN = "admin";
+
+    private String email;
+
+
+    private MongoDatabase database;
+
+    protected User(String email, MongoDatabase database) {
+        this.email = email;
+        this.database = database;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+}
