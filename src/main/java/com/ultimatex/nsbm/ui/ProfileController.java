f5755eba79c527104c611cacecd33128636c8281
lakinduakash <lakinduakash@gmail.com>
2018-05-24 21:45:10 +0530

    make Controller class Initialisable

--- src/main/java/com/ultimatex/nsbm/ui/ProfileController.java
+++ src/main/java/com/ultimatex/nsbm/ui/ProfileController.java
@@ -1,4 +1,16 @@
 package com.ultimatex.nsbm.ui;
 
-public class ProfileController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
 }
