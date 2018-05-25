77d7dd4f7b989a6938beae91f5a0ef0e9d12e629
lakinduakash <lakinduakash@gmail.com>
2018-05-25 14:50:31 +0530

    update test

--- src/test/java/com/ultimatex/nsbm/SessionTest.java
+++ src/test/java/com/ultimatex/nsbm/SessionTest.java
@@ -1,11 +1,15 @@
 package com.ultimatex.nsbm;
 
-import com.ultimatex.nsbm.util.*;
+import com.ultimatex.nsbm.util.DatabaseHelper;
+import com.ultimatex.nsbm.util.Session;
+import com.ultimatex.nsbm.util.StudentHelper;
+import com.ultimatex.nsbm.util.UserNotFoundException;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class SessionTest {
@@ -18,12 +22,13 @@ public class SessionTest {
 
     }
 
-    @Test(expected = InvalidSessionException.class)
+    @Test
     public void sessionClose() {
         Session.createInstance("email@email.com", "password");
         Session.close();
 
-        Session.getSession();
+        Session session = Session.getSession();
+        assertThat(session, is(nullValue()));
 
     }
 
