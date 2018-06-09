b324730ddafe609d263693c9691eb13134452339
lakinduakash <lakinduakash@gmail.com>
2018-06-10 00:53:42 +0530

    add admin main view controller

---
+++ src/main/java/com/ultimatex/nsbm/ui/admin/MainController.java
@@ -0,0 +1,39 @@
+package com.ultimatex.nsbm.ui.admin;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void onSignOutButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onRegisterButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onEditButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void onSettingsButtonClick(MouseEvent event) {
+
+    }
+
+
+}
