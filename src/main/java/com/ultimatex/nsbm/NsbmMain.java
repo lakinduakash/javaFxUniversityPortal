e83a29ab5e28827e1b3e505d56be85fb901739f1
lakinduakash <lakinduakash@gmail.com>
2018-06-16 02:07:58 +0530

    add ENTER button function to logging in

--- src/main/java/com/ultimatex/nsbm/NsbmMain.java
+++ src/main/java/com/ultimatex/nsbm/NsbmMain.java
@@ -3,8 +3,11 @@ package com.ultimatex.nsbm;
 
 import com.ultimatex.nsbm.ui.LoginController;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -21,9 +24,17 @@ public class NsbmMain extends Application {
 
         Scene scene = new Scene(loader.getRoot(), lc.loginPane.getPrefWidth(), lc.loginPane.getPrefHeight());
 
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER)
+                    lc.onLoginButtonClick(event);
+            }
+        });
+
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
-
 
         primaryStage.show();
     }
