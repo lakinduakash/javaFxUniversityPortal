56b523574fcd36522237d2c737d1c25537cf005f
lakinduakash <lakinduakash@gmail.com>
2018-06-16 14:00:56 +0530

    fix bugs

--- src/main/java/com/ultimatex/nsbm/util/GenerateIndex.java
+++ src/main/java/com/ultimatex/nsbm/util/GenerateIndex.java
@@ -9,6 +9,8 @@ public class GenerateIndex {
 
     private Datastore datastore;
 
+    private int lastIndex;
+
     public GenerateIndex() {
         datastore = MorphiaHelper.getInstance().getDataStore();
     }
@@ -19,7 +21,7 @@ public class GenerateIndex {
         LastIndex index = query.get();
 
         if (index == null) {
-            index = new LastIndex(1);
+            index = new LastIndex(0);
             datastore.save(index);
             return index.getIndex();
         } else {
@@ -28,18 +30,19 @@ public class GenerateIndex {
 
     }
 
-    public int genNewIndexAndSave() {
-        int lastIndex = getLastIndex();
-        Query<LastIndex> query = datastore.createQuery(LastIndex.class).field("index").equal(lastIndex);
+    public int genNewIndex() {
 
-        UpdateOperations<LastIndex> updateOperations = datastore.createUpdateOperations(LastIndex.class);
-
+        lastIndex = getLastIndex();
         lastIndex += 1;
-
-        updateOperations.set("index", lastIndex);
-        datastore.update(query, updateOperations);
         return lastIndex;
 
+    }
+
+    public void saveIndex() {
+        UpdateOperations<LastIndex> updateOperations = datastore.createUpdateOperations(LastIndex.class);
+        Query<LastIndex> query = datastore.createQuery(LastIndex.class).field("index").equal(lastIndex - 1);
+        updateOperations.set("index", lastIndex);
+        datastore.update(query, updateOperations);
     }
 
 }
