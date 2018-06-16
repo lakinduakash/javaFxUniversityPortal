360df55a39428119a1672e19c1e459c3e7b39347
lakinduakash <lakinduakash@gmail.com>
2018-06-17 01:02:27 +0530

    add new functions

--- src/main/java/com/ultimatex/nsbm/model/crud/StudentImpl.java
+++ src/main/java/com/ultimatex/nsbm/model/crud/StudentImpl.java
@@ -6,6 +6,8 @@ import com.ultimatex.nsbm.model.Student;
 import com.ultimatex.nsbm.util.MorphiaHelper;
 import org.mongodb.morphia.Datastore;
 
+import java.util.ArrayList;
+
 public class StudentImpl {
 
     private Datastore datastore;
@@ -24,4 +26,14 @@ public class StudentImpl {
         }
 
     }
+
+    public Student getStudentByIndex(String index) {
+        return datastore.createQuery(Student.class).field("indexNumber").equal(index).get();
+    }
+
+    public ArrayList<Student> getStudentByName(String name) {
+        return new ArrayList<>(datastore.createQuery(Student.class).field("name").equal(name).asList());
+    }
+
+
 }
