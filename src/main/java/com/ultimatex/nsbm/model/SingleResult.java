2aa7b57002e4a09b0fc5144e7a7174b28f990f12
lakinduakash <lakinduakash@gmail.com>
2018-06-19 14:11:10 +0530

    add final variables for grades

--- src/main/java/com/ultimatex/nsbm/model/SingleResult.java
+++ src/main/java/com/ultimatex/nsbm/model/SingleResult.java
@@ -4,6 +4,16 @@ import jdk.nashorn.internal.ir.annotations.Reference;
 
 public class SingleResult {
 
+    public static final String A_PLUS = "A+";
+    public static final String A_ = "A";
+    public static final String A_MINUS = "A-";
+    public static final String B_PLUS = "B+";
+    public static final String B_ = "B";
+    public static final String B_MINUS = "B-";
+    public static final String C_PLUS = "C+";
+    public static final String C_ = "C";
+    public static final String C_MINUS = "C-";
+
     @Reference
     private Subject subject;
     private String grade;
