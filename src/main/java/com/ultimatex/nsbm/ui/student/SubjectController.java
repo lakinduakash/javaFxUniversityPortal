afc5477456db8dd3205e1034ccb72982143da2e6
lakinduakash <lakinduakash@gmail.com>
2018-06-19 21:42:25 +0530

    initialise field ids

--- src/main/java/com/ultimatex/nsbm/ui/student/SubjectController.java
+++ src/main/java/com/ultimatex/nsbm/ui/student/SubjectController.java
@@ -1,4 +1,150 @@
 package com.ultimatex.nsbm.ui.student;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+
 public class SubjectController {
+
+    @FXML
+    private AnchorPane subjectPane;
+
+    @FXML
+    private Tab tabYear1;
+
+    @FXML
+    private AnchorPane anchorPaneYear1;
+
+    @FXML
+    private JFXListView<?> listViewy1s1c;
+
+    @FXML
+    private JFXListView<?> listViewy1s1o;
+
+    @FXML
+    private JFXButton buttony1s1;
+
+    @FXML
+    private JFXListView<?> listViewy1s2c;
+
+    @FXML
+    private JFXListView<?> listViewy1s2o;
+
+    @FXML
+    private JFXButton buutony1s2;
+
+    @FXML
+    private Tab tabYear2;
+
+    @FXML
+    private AnchorPane anchorPaneYear2;
+
+    @FXML
+    private JFXListView<?> listViewy2s1c;
+
+    @FXML
+    private JFXListView<?> listViewy2s1o;
+
+    @FXML
+    private JFXButton buttony2s1;
+
+    @FXML
+    private JFXListView<?> listViewy2s2c;
+
+    @FXML
+    private JFXListView<?> listViewy2s2o;
+
+    @FXML
+    private JFXButton buutony2s2;
+
+    @FXML
+    private Tab tabYear3;
+
+    @FXML
+    private AnchorPane anchorPaneYear3;
+
+    @FXML
+    private JFXListView<?> listViewy3s1c;
+
+    @FXML
+    private JFXListView<?> listViewy3s1o;
+
+    @FXML
+    private JFXButton buttony3s1;
+
+    @FXML
+    private JFXListView<?> listViewy3s2c;
+
+    @FXML
+    private JFXListView<?> listViewy3s2o;
+
+    @FXML
+    private JFXButton buutony3s2;
+
+    @FXML
+    private Tab tabYear4;
+
+    @FXML
+    private AnchorPane anchorPaneYear4;
+
+    @FXML
+    private JFXListView<?> listViewy4s1c;
+
+    @FXML
+    private JFXListView<?> listViewy4s1o;
+
+    @FXML
+    private JFXButton buttony4s1;
+
+    @FXML
+    private JFXListView<?> listViewy4s2c;
+
+    @FXML
+    private JFXListView<?> listViewy4s2o;
+
+    @FXML
+    private JFXButton buutony4s2;
+
+    @FXML
+    void onClickSaveButtonSubjectY1S1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY1S2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY2S1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY2S2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY3S1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY3S2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY4S1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSaveButtonSubjectY4S2(ActionEvent event) {
+
+    }
 }
