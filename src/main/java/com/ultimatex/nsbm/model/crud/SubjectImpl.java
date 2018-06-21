ee03801335c57ffcffb2493c73fb02260cc9df22
lakinduakash <lakinduakash@gmail.com>
2018-06-21 21:28:44 +0530

    crud class for Subject

---
+++ src/main/java/com/ultimatex/nsbm/model/crud/SubjectImpl.java
@@ -0,0 +1,37 @@
+package com.ultimatex.nsbm.model.crud;
+
+import com.ultimatex.nsbm.model.Subject;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.Datastore;
+
+public class SubjectImpl extends BaseImpl<Subject> {
+
+    private Datastore datastore;
+
+    public SubjectImpl() {
+        super();
+        datastore = getDatastore();
+    }
+
+    @Override
+    public boolean insert(Subject subject) {
+        datastore.save(subject);
+        return false;
+    }
+
+    @Override
+    public boolean update(ObjectId id, Subject updated) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(ObjectId id) {
+        int i = datastore.delete(datastore.createQuery(Subject.class).field("_id").equal(id)).getN();
+        return i != 0;
+    }
+
+    @Override
+    public Subject find(String key, Object value) {
+        return datastore.createQuery(Subject.class).field("_id").equal(value).get();
+    }
+}
