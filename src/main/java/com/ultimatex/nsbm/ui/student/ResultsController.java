9f1f84e453f73d87c05eed76605cbbda997589e7
lakinduakash <lakinduakash@gmail.com>
2018-06-22 12:37:50 +0530

    refer from FXML

--- src/main/java/com/ultimatex/nsbm/ui/student/ResultsController.java
+++ src/main/java/com/ultimatex/nsbm/ui/student/ResultsController.java
@@ -1,4 +1,75 @@
 package com.ultimatex.nsbm.ui.student;
 
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTabPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ResultsController {
+    @FXML
+    private JFXTabPane resultPane;
+
+    @FXML
+    private JFXListView<?> listViewResy1s1;
+
+    @FXML
+    private JFXListView<?> listViewResy1s2;
+
+    @FXML
+    private JFXListView<?> listViewResy2s1;
+
+    @FXML
+    private JFXListView<?> listViewResy2s2;
+
+    @FXML
+    private JFXListView<?> listViewResy3s1;
+
+    @FXML
+    private JFXListView<?> listViewResy3s2;
+
+    @FXML
+    private JFXListView<?> listViewResy4s1;
+
+    @FXML
+    private JFXListView<?> listViewResy4s2;
+
+    @FXML
+    void onSaveButtonClicky1s1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky1s2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky2s1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky2s2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky3s1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky3s2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky4s1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveButtonClicky4s2(ActionEvent event) {
+
+    }
 }
