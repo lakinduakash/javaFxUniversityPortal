6cbac46288f8077648a3a875b0bb01bdceaccd16
lakinduakash <lakinduakash@gmail.com>
2018-08-07 01:58:34 +0530

    add results to list view

--- src/main/java/com/ultimatex/nsbm/ui/student/ResultsController.java
+++ src/main/java/com/ultimatex/nsbm/ui/student/ResultsController.java
@@ -2,14 +2,21 @@ package com.ultimatex.nsbm.ui.student;
 
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTabPane;
+import com.ultimatex.nsbm.GlobalState;
+import com.ultimatex.nsbm.model.Course;
+import com.ultimatex.nsbm.model.SingleResult;
+import com.ultimatex.nsbm.model.Student;
+import com.ultimatex.nsbm.model.Subject;
 import com.ultimatex.nsbm.ui.ResultViewItemController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ResultsController implements Initializable {
@@ -75,10 +82,15 @@ public class ResultsController implements Initializable {
 
     }
 
+    Student selectedStudent = GlobalState.getSelectedStudent();
+    Course selectedCourse = GlobalState.getSelectedCourse();
+
     @FXML
     void onSaveButtonClicky4s2(ActionEvent event) {
 
+
     }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -86,6 +98,36 @@ public class ResultsController implements Initializable {
         listViewResy1s1.getItems().add(initOneItem().getResultViewItem());
         listViewResy1s1.getItems().add(initOneItem().getResultViewItem());
     }
+
+    void initTab1() {
+
+    }
+
+
+    private void addResultsToList(ArrayList<SingleResult> cResults, ArrayList<Subject> cSubject, JFXListView<HBox> listView) {
+        for (Subject s : cSubject) {
+            ResultViewItemController rc = initOneItem();
+            rc.setLabelSubjectName(new Label(s.getName()));
+            rc.setCustomObject(s);
+
+            for (SingleResult r : cResults) {
+                if (r.getSubject().equals(s)) {
+                    rc.getComboBoxGradeSelector().setValue(r.getGrade());
+                }
+            }
+            listView.getItems().add(rc.getResultViewItem());
+        }
+    }
+
+
+    private ArrayList<SingleResult> getUpdateResultValue(JFXListView<HBox> item) {
+        for (HBox h : item.getItems()) {
+        }
+
+        return null;
+    }
+
+
 
     private ResultViewItemController initOneItem() {
 
@@ -99,4 +141,5 @@ public class ResultsController implements Initializable {
 
         return loader.getController();
     }
+
 }
