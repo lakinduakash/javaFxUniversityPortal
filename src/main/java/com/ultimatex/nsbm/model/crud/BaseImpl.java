771e58919bd57d33b29dcaefa6a6710dd2d16bf1
lakinduakash <lakinduakash@gmail.com>
2018-08-23 03:00:03 +0530

    add doc

--- src/main/java/com/ultimatex/nsbm/model/crud/BaseImpl.java
+++ src/main/java/com/ultimatex/nsbm/model/crud/BaseImpl.java
@@ -19,14 +19,37 @@ public abstract class BaseImpl<T> {
         datastore = MorphiaHelper.getInstance().getDataStore();
     }
 
+    /**
+     * Insert new value
+     *
+     * @param t insert new t value to database
+     * @return returns true if successfully inserted
+     */
     public abstract boolean insert(T t);
 
+    /**
+     * Update existing value
+     * @param id object id
+     * @param updated updated value. ObjectId should be same
+     * @return returns true if successfully updated
+     */
     public abstract boolean update(ObjectId id, T updated);
 
     public abstract boolean update(T t);
 
+    /**
+     * Delete value from database
+     * @param id ObjectId to delete
+     * @return returns true if successfully deleted
+     */
     public abstract boolean delete(ObjectId id);
 
+    /**
+     * Find specific value from database
+     * @param key key of the data
+     * @param value value of the data
+     * @return returns List of data which matches to key/value pair
+     */
     public abstract ArrayList<T> find(String key, Object value);
 
     protected Datastore getDatastore() {
