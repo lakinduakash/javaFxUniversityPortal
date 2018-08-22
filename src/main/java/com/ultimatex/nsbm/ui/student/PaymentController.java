d8aff16b47979ce0a69a0943243a874bce246774
lakinduakash <lakinduakash@gmail.com>
2018-08-23 01:44:27 +0530

    add FXML attributes

--- src/main/java/com/ultimatex/nsbm/ui/student/PaymentController.java
+++ src/main/java/com/ultimatex/nsbm/ui/student/PaymentController.java
@@ -1,4 +1,84 @@
 package com.ultimatex.nsbm.ui.student;
 
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTabPane;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class PaymentController {
+
+    @FXML
+    private JFXTabPane paymentPane;
+
+    @FXML
+    private Label labelY1S1Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY1s1;
+
+    @FXML
+    private Label labelY1s1PaidStatus;
+
+    @FXML
+    private Label labelY1s2Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY1s2;
+
+    @FXML
+    private Label labelY1s2PaidStatus;
+
+    @FXML
+    private Label labelY2S1Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY2s1;
+
+    @FXML
+    private Label labelY2s1PaidStatus;
+
+    @FXML
+    private Label labelY2s2Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY2s2;
+
+    @FXML
+    private Label labelY2s2PaidStatus;
+
+    @FXML
+    private Label labelY3S1Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY3s1;
+
+    @FXML
+    private Label labelY3s1PaidStatus;
+
+    @FXML
+    private Label labelY3s2Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY3s2;
+
+    @FXML
+    private Label labelY3s2PaidStatus;
+
+    @FXML
+    private Label labelY4S1Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY4s1;
+
+    @FXML
+    private Label labelY4s1PaidStatus;
+
+    @FXML
+    private Label labelY4s2Total;
+
+    @FXML
+    private JFXCheckBox checkBoxY4s2;
+
+    @FXML
+    private Label labelY4s2PaidStatus;
 }
