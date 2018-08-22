f8d22a5f561fa41b91e44cac644f99a33f4af6d7
lakinduakash <lakinduakash@gmail.com>
2018-08-22 12:24:25 +0530

    add lecturer class

---
+++ src/main/java/com/ultimatex/nsbm/model/Lecture.java
@@ -0,0 +1,13 @@
+package com.ultimatex.nsbm.model;
+
+public class Lecture {
+    public final static String TYPE_LEC = "Lecturer";
+    public final static String TYPE_INS = "Instructor";
+
+    private String name;
+    private String type;
+    private int id;
+    private String dept;
+
+
+}
