a12ac4352264a1a313471019e19342314fdf4d5c
lakinduakash <lakinduakash@gmail.com>
2018-08-22 15:01:11 +0530

    Add controller class for view all students

--- src/main/java/com/ultimatex/nsbm/ui/admin/ViewAllController.java
+++ src/main/java/com/ultimatex/nsbm/ui/admin/ViewAllController.java
@@ -1,11 +1,20 @@
 package com.ultimatex.nsbm.ui.admin;
 
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewAllController implements Initializable {
+
+    @FXML
+    private JFXListView<?> listViewUN;
+
+    @FXML
+    private JFXListView<?> listViewPS;
+
     /**
      * Called to initialize a controller after its root element has been
      * completely processed.
