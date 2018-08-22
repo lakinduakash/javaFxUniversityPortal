4873cbeadc1d864a7e6405e1b2a33735e11c0cae
lakinduakash <lakinduakash@gmail.com>
2018-08-23 02:18:07 +0530

    update Payment class

--- src/main/java/com/ultimatex/nsbm/ui/student/PaymentController.java
+++ src/main/java/com/ultimatex/nsbm/ui/student/PaymentController.java
@@ -2,10 +2,18 @@ package com.ultimatex.nsbm.ui.student;
 
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTabPane;
+import com.ultimatex.nsbm.GlobalState;
+import com.ultimatex.nsbm.model.Student;
+import com.ultimatex.nsbm.model.Subject;
+import com.ultimatex.nsbm.model.crud.CourseImpl;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class PaymentController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PaymentController implements Initializable {
 
     @FXML
     private JFXTabPane paymentPane;
@@ -20,7 +28,7 @@ public class PaymentController {
     private Label labelY1s1PaidStatus;
 
     @FXML
-    private Label labelY1s2Total;
+    private Label labelY1S2Total;
 
     @FXML
     private JFXCheckBox checkBoxY1s2;
@@ -38,7 +46,7 @@ public class PaymentController {
     private Label labelY2s1PaidStatus;
 
     @FXML
-    private Label labelY2s2Total;
+    private Label labelY2S2Total;
 
     @FXML
     private JFXCheckBox checkBoxY2s2;
@@ -56,7 +64,7 @@ public class PaymentController {
     private Label labelY3s1PaidStatus;
 
     @FXML
-    private Label labelY3s2Total;
+    private Label labelY3S2Total;
 
     @FXML
     private JFXCheckBox checkBoxY3s2;
@@ -74,11 +82,151 @@ public class PaymentController {
     private Label labelY4s1PaidStatus;
 
     @FXML
-    private Label labelY4s2Total;
+    private Label labelY4S2Total;
 
     @FXML
     private JFXCheckBox checkBoxY4s2;
 
     @FXML
     private Label labelY4s2PaidStatus;
+
+    Student selectedStudent=GlobalState.getSelectedStudent();
+
+    public PaymentController()
+    {
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initTotalPayments(CourseImpl.Y1S1);
+        initTotalPayments(CourseImpl.Y1S2);
+        initTotalPayments(CourseImpl.Y2S1);
+        initTotalPayments(CourseImpl.Y2S2);
+        initTotalPayments(CourseImpl.Y3S1);
+        initTotalPayments(CourseImpl.Y3S2);
+        initTotalPayments(CourseImpl.Y4S1);
+        initTotalPayments(CourseImpl.Y4S2);
+
+        labelY1s1PaidStatus.setText("Paid");
+    }
+
+    private void initTotalPayments(String sem)
+    {
+        switch (sem)
+        {
+            case CourseImpl.Y1S1:
+                int total=0;
+                for(Subject s:selectedStudent.getCourse().getYear1sem1())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy1s1())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY1S1Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y1S2:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear1sem2())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy1s2())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY1S2Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y2S1:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear2sem1())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy2s1())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY2S1Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y2S2:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear2sem2())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy2s2())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY2S2Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y3S1:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear3sem1())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy3s1())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY3S1Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y3S2:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear3sem2())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy3s2())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY3S2Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y4S1:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear4sem1())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy4s1())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY4S1Total.setText(Integer.toString(total)+".00");
+                break;
+            case CourseImpl.Y4S2:
+                total=0;
+                for(Subject s:selectedStudent.getCourse().getYear4sem2())
+                {
+                    total+=s.getPrice();
+                }
+                for(Subject s:selectedStudent.getSelectedSubjectsy4s2())
+                {
+                    total+=s.getPrice();
+                }
+
+                labelY4S2Total.setText(Integer.toString(total)+".00");
+                break;
+        }
+    }
+
 }
