0847605e21bc72233eb0d5ad5286d788970f8e65
lakinduakash <lakinduakash@gmail.com>
2018-08-23 02:22:22 +0530

    add insert lec

--- src/main/java/com/ultimatex/nsbm/model/crud/LecturerImpl.java
+++ src/main/java/com/ultimatex/nsbm/model/crud/LecturerImpl.java
@@ -1,16 +1,30 @@
 package com.ultimatex.nsbm.model.crud;
 
 import com.ultimatex.nsbm.model.Lecture;
+import com.ultimatex.nsbm.util.MorphiaHelper;
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.Datastore;
 
 import java.util.ArrayList;
 
 public class LecturerImpl extends BaseImpl<Lecture> {
 
 
+    private Datastore datastore;
+
+    public LecturerImpl() {
+        super();
+        datastore = MorphiaHelper.getInstance().getDataStore();
+    }
+
     @Override
     public boolean insert(Lecture lecture) {
-        return false;
+        try {
+            datastore.save(lecture);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
