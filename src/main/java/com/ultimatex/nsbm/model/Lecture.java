bd506db54255713e7b51cf714d9092179f8094d5
lakinduakash <lakinduakash@gmail.com>
2018-08-22 12:25:26 +0530

    edit lecturer class

--- src/main/java/com/ultimatex/nsbm/model/Lecture.java
+++ src/main/java/com/ultimatex/nsbm/model/Lecture.java
@@ -1,12 +1,18 @@
 package com.ultimatex.nsbm.model;
 
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
+
 public class Lecture {
     public final static String TYPE_LEC = "Lecturer";
     public final static String TYPE_INS = "Instructor";
 
     private String name;
     private String type;
+
+    @Indexed(options = @IndexOptions(unique = true))
     private int id;
+
     private String dept;
 
 
