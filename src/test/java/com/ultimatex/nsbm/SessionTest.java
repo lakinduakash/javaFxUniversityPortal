e7be4e89a158d9580370cd370d876cf05cc6bbee
lakinduakash <lakinduakash@gmail.com>
2018-08-28 00:14:21 +0530

    update tests

--- src/test/java/com/ultimatex/nsbm/SessionTest.java
+++ src/test/java/com/ultimatex/nsbm/SessionTest.java
@@ -2,12 +2,10 @@ package com.ultimatex.nsbm;
 
 import com.ultimatex.nsbm.util.DatabaseHelper;
 import com.ultimatex.nsbm.util.Session;
-import com.ultimatex.nsbm.util.StudentHelper;
 import com.ultimatex.nsbm.util.UserNotFoundException;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
@@ -34,11 +32,7 @@ public class SessionTest {
 
     @Test
     public void getUserOfStudentHelperWithValidAccount() {
-        Session.createInstance("email@email.com", "password");
 
-        User user = Session.getUser(Session.getSession(), DatabaseHelper.getInstance().getDatabase());
-
-        assertThat(user, is(instanceOf(StudentHelper.class)));
     }
 
     @Test(expected = UserNotFoundException.class)
